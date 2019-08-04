bb3877891b1365bef5991abb95f8ba87640f4a87
jibin george <hello@jibingeo.com>
2019-08-04 18:14:19 +0530

    fix keymap

--- keyboards/xd75/keymaps/jibingeo/keymap.c
+++ keyboards/xd75/keymaps/jibingeo/keymap.c
@@ -64,9 +64,9 @@ const uint16_t PROGMEM keymaps[][MATRIX_ROWS][MATRIX_COLS] = {
 
  [L1] = { /* FUNCTION */
   { KC_F1,    KC_F2,   KC_F3,   KC_F4,   KC_F5,   KC_F6,   _______, _______, _______, KC_F7,   KC_F8,   KC_F9,   KC_F10,  KC_F11,  KC_F12  },
-  { WN_NEXT,  KC_1,    KC_2,    KC_3,    KC_4,    KC_5,    _______, _______, _______, KC_6,    KC_7,    KC_8,    KC_9,    KC_0,    _______ },
-  { WN_PREV,  KC_TILD, KC_EQL,  KC_QUOT, KC_DQUO, KC_MINS, _______, _______, _______, KC_PLUS, KC_UNDS, KC_BSLS, KC_PIPE, KC_COLN, _______ },
-  { _______,  _______, _______, _______, _______, _______, _______, _______, _______, _______, _______, _______, _______, _______, _______ },
+  { WN_NEXT,  _______, _______, KC_LBRC, KC_RBRC, KC_QUOT, _______, _______, _______, KC_UNDS, KC_7,    KC_8,    KC_9,    KC_0,    _______ },
+  { WN_PREV,  _______, _______, KC_LCBR, KC_RCBR, KC_DQUO, _______, _______, _______, KC_MINS, KC_4,    KC_5,    KC_6,    _______, _______ },
+  { _______,  _______, _______, KC_LABK, KC_RABK, KC_EQL,  _______, _______, _______, KC_PLUS, KC_1,    KC_2,    KC_3,    _______, _______ },
   { RGB_RMOD, RGB_MOD, RGB_TOG, _______, _______, _______, _______, _______, _______, _______, _______, _______, _______, _______, RESET   },
  },
 
